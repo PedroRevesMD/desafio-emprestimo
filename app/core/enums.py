904bf43e8d362684b0719b009ab106b990e421fb
PedroRevesMD <pedroreves11@gmail.com>
2026-03-14 20:34:27 -0300

    feat: add core entities

---
+++ app/core/enums.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class LoanTypeEnum(str, Enum):
+    PERSONAL = "PERSONAL"
+    GUARANTEED = "GUARANTEED"
+    CONSIGNMENT = "CONSIGNMENT"
+
+LOAN_RATES = {
+    LoanTypeEnum.PERSONAL: 4,
+    LoanTypeEnum.GUARANTEED: 3,
+    LoanTypeEnum.CONSIGNMENT: 2,
+}
+
+VALID_UFS = {
+    "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA",
+    "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+    "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+}
